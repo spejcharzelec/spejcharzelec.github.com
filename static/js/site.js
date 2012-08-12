b691a09bec284e49b8e39ab164a549fce6b38ebf
Tomáš Horáček <tomas.horacek@gmail.com>
2012-08-12 06:50:57 +0200

    add lightbox lib and support in vystavy layout

--- static/js/site.js
+++ static/js/site.js
@@ -3,4 +3,16 @@
         $(this).siblings('.hide').slideToggle();
         return false;
     });
+    
+    var lightbox_images_root = '/static/js/lib/jquery-lightbox/';
+    var lightbox_settings = {
+        imageLoading: lightbox_images_root + 'images/lightbox-ico-loading.gif',
+        imageBtnPrev: lightbox_images_root + 'images/lightbox-btn-prev.gif',
+        imageBtnNext: lightbox_images_root + 'images/lightbox-btn-next.gif',
+        imageBtnClose: lightbox_images_root + 'images/lightbox-btn-close.gif',
+        imageBlank: lightbox_images_root + 'images/lightbox-blank.gif'
+    };
+    $("ul.photo-list").each(function() {
+        $("a.lightbox", this).lightBox(lightbox_settings);
+    });
 })(jQuery);
